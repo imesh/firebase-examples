6a636f8c5fe17751c4dc656da10db9f10153a8e9
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2018-06-26 14:50:36 +0530

    First commit

---
+++ react-firebase-database/README.md
@@ -0,0 +1,42 @@
+# React Firebase Database Example
+
+This sample application demonstrates the ability to connect to Firebase databases via a React.js web application
+
+## How to Run
+
+- Create a Firebase account at https://firebase.google.com/
+
+- Obtain credentials from the Firebase console: "Firebase -> Project Settings -> Add Firebase to your web app":
+
+  ````json
+  // Initialize Firebase
+  var config = {
+      apiKey: "apiKey",
+      authDomain: "projectId.firebaseapp.com",
+      databaseURL: "https://databaseName.firebaseio.com",
+      projectId: "projectId",
+      storageBucket: "bucket.appspot.com",
+      messagingSenderId: "messagingSenderId"
+  };
+  ````
+
+- Update above credentials in the src/App.js file.
+
+- Install dependencies:
+  
+  ````bash
+  npm install
+  ````
+
+- Start the web application:
+  
+  ````bash
+  npm start
+  ````
+
+- Log into Firebase console, navigate to the Database section, and create a new entry called "speed" in the Realtime database with a numeric value.
+
+- Now, check the home page and JavaScript console of the above web application. If everything went well, the value entered in the Firebase console should reflect in the web application.
+
+## License
+Licensed under Apache 2.0
---
+++ react-firebase-database/src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './App.css';
+import firebase from 'firebase';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      speed: 10
+    }
+    // Initialize Firebase
+    var config = {
+      apiKey: "apiKey",
+      authDomain: "projectId.firebaseapp.com",
+      databaseURL: "https://databaseName.firebaseio.com",
+      projectId: "projectId",
+      storageBucket: "bucket.appspot.com",
+      messagingSenderId: "messagingSenderId"
+    };
+    firebase.initializeApp(config);
+  }
+
+  componentDidMount() {
+    const speedRef = firebase.database().ref('speed');
+    speedRef.on('value', snap => {
+      console.log('---> Reading speed value: ' + snap.val());
+      this.setState({
+        speed: snap.val()
+      });
+    });
+
+    const user = {
+      username: 'foo',
+      email: 'foo@bar.org',
+      profile_picture : 'https://foo/profile/image'
+    }
+    console.log('---> Writing new JSON document: ' + JSON.stringify(user));
+    firebase.database().ref('users/' + '1').set(user, 
+      function(error) {
+        if (error) {
+          console.log('Error: ' + error);
+        } else {
+          console.log('Data saved successfully!');
+        }
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.speed}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
